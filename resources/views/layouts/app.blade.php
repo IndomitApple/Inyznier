c368e2123c638b766a2c8a265749bcde4e47d56e
Konrad Przytula <kondzio2104@gmail.com>
2021-11-28 15:15:39 +0100

    Frontend at welcome.blade.php - main site

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('template/dist/css/theme.min.css')}}">
 </head>
 <body>
     <div id="app">
