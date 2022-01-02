9733d560b84e80c87830b98118f38b63602dba25
Konrad Przytula <kondzio2104@gmail.com>
2022-01-02 16:45:56 +0100

    Prescription table created, prescription form.

--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -10,7 +10,7 @@ class PrescriptionController extends Controller
     public function index()
     {
         date_default_timezone_set('Europe/Warsaw');
-        $bookings = Booking::where('date',date('Y-m-d'))->where('status',1)->get();
+        $bookings = Booking::where('date',date('Y-m-d'))->where('status',0)->get();
         return view('prescription.index',compact('bookings'));
 
     }
